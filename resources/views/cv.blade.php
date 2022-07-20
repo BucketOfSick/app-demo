da8350d6271be8faf67c9d762c363eaa5c818722
Patrickvdh <patrickvanderheide04@gmail.com>
2022-07-20 21:43:58 +0200

    V8.3 Portfolio V1.2

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -3,13 +3,22 @@
 @section('title', 'CV')
 
 @section('content')
-<div class="max-w-6xl mx-auto sm:px-6 lg:px-8 unitext">
-    <div class="flex justify-start pt-8 sm:justify-start sm:pt-0">
-        <h1>CV</h1>        
+<div class="container unitext cvcontent">
+  <div class="row cvrow">
+    <div class="col-2-sm cvcol">
+        <h3>Profiel</h3><p>Geboortedatum: 15-01-2004<br><br>Geboorteplaats: Assen<br><br>Nationaliteit: Nederlands<br><br>Email: patrickvanderheide83@gmail.com<br><br><b>Talen:</b> <br><br> Nederlands: moedertaal<br><br>Engels: Zeer vaardig</b></p>
     </div>
-
-    <div class="mt-8 overflow-hidden unitext">
-        <p>This is the about page</p>    
+    <div class="col-2-sm cvcol">
+        <h3>Werkervaring</h3><p><b>Goederenverwerker, Jumbo<br>Feb 2020 - Heden</b><br><br>Sinds februari 2020 ben ik werkzaam <br>bij Jumbo als goederenverwerker.<br></p>
     </div>
+    <div class="w-200"></div>
+    <div class="col-2-sm cvcol">
+        <h3>Opleiding</h3><p><b>MBO Software Developer, Alfa-College<br>Sep 2020 - Heden</b><br><br>Sinds september 2020 volg ik de opleiding<br> MBO Software Developer op het Alfa-College<br> in Groningen aan de Boumaboulevard.
+        <br><br><b>VMBO/TL, Gomarus College<br>Sep 2016 - Apr 2020</b> <br><br>In April 2020 heb ik mijn diploma voor VMBO/TL<br> behaald op het Gomarus College in Assen.</p>    
+    </div>
+    <div class="col-2-sm cvcol">
+        <h3>Vaardigheden</h3><p>HTML/CSS: Vaardig<br><br>MySQL: Vaardig<br><br>Javascript: Vaardig<br><br>PHP: Beginner<br><br>Laravel: Beginner<br><br>C#: Beginner</p>      
+    </div>
+  </div>
 </div>
 @endsection
